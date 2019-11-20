ef7ae0ee2b74dd02af0710699e1d004503e77885
JackMD <jackmtaylor.jmt@gmail.com>
2019-11-20 21:48:40 +0500

    add license headers

--- src/JackMD/Locale/Locale.php
+++ src/JackMD/Locale/Locale.php
@@ -1,6 +1,32 @@
 <?php
 declare(strict_types = 1);
 
+/**
+ * Locale
+ * 
+ * Locale, a virion for PocketMine-MP.
+ * Copyright (c) 2019 JackMD  < https://github.com/JackMD/Locale >
+ *
+ * Website: https://jacktaylor.cc
+ * Discord: JackMD#3717
+ * Twitter: JackMTaylor_
+ *
+ * This software is distributed under "GNU General Public License v3.0".
+ * This license allows you to use it and/or modify it but you are not at
+ * all allowed to sell this plugin at any cost. If found doing so the
+ * necessary action required would be taken.
+ *
+ * Locale is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3.0 for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program. If not, see
+ * <https://opensource.org/licenses/GPL-3.0>.
+ * -----------------------------------------------------------------------
+ */
+
 namespace JackMD\Locale;
 
 use JackMD\Locale\Exceptions\ConfigException;
